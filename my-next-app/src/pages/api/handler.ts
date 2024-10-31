74576cc1fb8d3f5f948ddf4af790a7e9c60b8cb1
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-31 10:32:48 -0400

    s

--- my-next-app/src/pages/api/handler.ts
+++ my-next-app/src/pages/api/handler.ts
@@ -1,15 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
+//const connectionString = process.env.POSTGRES_URL_NON_POOLING;
 import { createClient } from '@vercel/postgres';
+
+
 
  
 export async function queryvideo() {
-
-  const connectionString = process.env.POSTGRES_URL_NON_POOLING;
-  const client = createClient({connectionString});
+  
+  const connectionString = "postgres://default:RkmTvCKx9h3L@ep-curly-block-a4vx6vd3.us-east-1.aws.neon.tech/verceldb?sslmode=require";
+  const client = createClient({
+    connectionString,
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  });
   await client.connect();
+  console.log(client)
  
   try {
     const {rows} =
       await client.sql`SELECT * FROM Video`;
+      console.log(rows)
       return rows;
   } finally {
     await client.end();

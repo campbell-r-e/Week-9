db4badf9ef98846a9fcc7ebbfab126769bbd4bfc
bsucs <122378640+bsucs@users.noreply.github.com>
2024-10-30 23:42:20 -0400

    dfw

--- my-next-app/src/pages/api/handler.ts
+++ my-next-app/src/pages/api/handler.ts
@@ -2,13 +2,14 @@ import { createClient } from '@vercel/postgres';
 
  
 export async function queryvideo() {
-  const connectionString = "postgres://default:RkmTvCKx9h3L@ep-curly-block-a4vx6vd3.us-east-1.aws.neon.tech/verceldb?sslmode=require";
+
+  const connectionString = process.env.POSTGRES_URL_NON_POOLING;
   const client = createClient({connectionString});
   await client.connect();
  
   try {
     const {rows} =
-      await client.sql`SELECT * FROM "Video"`;
+      await client.sql`SELECT * FROM Video`;
       return rows;
   } finally {
     await client.end();

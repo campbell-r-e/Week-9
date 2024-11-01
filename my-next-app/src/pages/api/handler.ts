e87d409ffe1d46962b792566a115fab033c626ec
bsucs <122378640+bsucs@users.noreply.github.com>
2024-11-01 13:00:43 -0400

    df

--- my-next-app/src/pages/api/handler.ts
+++ my-next-app/src/pages/api/handler.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
  
 const prisma = new PrismaClient();
  
-export default async function datapuller(req,res) {
+export default async function datapuller(res) {
   try {
     const data = await prisma.video.findMany();
     res.status(200).json(data);

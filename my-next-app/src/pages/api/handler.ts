56c5697aa669102a37875b86b144a6513ea537d9
bsucs <122378640+bsucs@users.noreply.github.com>
2024-11-01 13:23:58 -0400

    e

--- my-next-app/src/pages/api/handler.ts
+++ my-next-app/src/pages/api/handler.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
  
 const prisma = new PrismaClient();
  
-export default async function datapuller(res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { id: number; name: string; url: string; votes: number; length: number; }[]): void; new(): any; }; }; }) {
+export default async function datapuller(res) {
   try {
     const data = await prisma.video.findMany();
     res.status(200).json(data);
